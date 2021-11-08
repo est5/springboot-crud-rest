c33f2902d4f64e6a9e8f41b9ed25a261b43d68a9
EST <ghostrekon5500@ya.ru>
2021-11-08 17:05:49 +0300

    init

---
+++ src/test/java/com/msrs/mechanicservice/MechanicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msrs.mechanicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
